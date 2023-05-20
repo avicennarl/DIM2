34f7b869586133b7f0c108abe00ab13c3af605e5
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-05-21 02:43:36 +0700

    Create 03-Potensio-toLED.ino
    NOT THE FINAL CODE!!!

---
+++ Arduino言語/03-Potensio-toLED.ino
@@ -0,0 +1,29 @@
+int ledtest=10;
+int readvalue=0;
+int ledvalue=0;
+
+void setup(){
+  pinMode(ledtest, OUTPUT);
+}
+
+void loop(){
+  readvalue= analogRead(A1);
+  ledvalue= map(readvalue, 0, 1024, 0, 255);
+  analogWrite(ledtest, ledvalue);
+}
+
+int led = 9;
+int pinPot = A1; 
+int nilaiPot = 0;
+  
+void setup() { 
+  pinMode(led,OUTPUT); 
+  Serial.begin(9600);
+}
+
+void loop() {
+  nilaiPot = analogRead(pinPot);
+  nilaiPot = map(nilaiPot, 0, 1023, 0, 255);
+  analogWrite(led, nilaiPot); Serial.print("nilai ADC: ");
+  Serial.println(nilaiPot);
+}
