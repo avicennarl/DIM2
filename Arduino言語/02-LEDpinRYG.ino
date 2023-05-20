8f34470a91018b9c6924ecab52eb9ed494993875
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-05-21 02:22:08 +0700

    Create 02-LEDpinRYG.ino

---
+++ Arduino言語/02-LEDpinRYG.ino
@@ -0,0 +1,37 @@
+int ledPinR=2;
+int ledPinY=3;
+int ledPinG=4;
+
+
+void setup()
+{
+    pinMode(ledPinR,OUTPUT);
+    pinMode(ledPinY,OUTPUT);
+    pinMode(ledPinG,OUTPUT);//Set the digital 8 port mode, OUTPUT: Output mode
+
+}
+
+void loop()
+{  
+
+    digitalWrite(ledPinR,HIGH); //HIGH is set to about 5V PIN8
+    delay(1000);               //Set the delay time, 1000 = 1S
+    digitalWrite(ledPinR,LOW);  //LOW is set to about 5V PIN8
+    delay(1000);
+    digitalWrite(ledPinY,HIGH); //HIGH is set to about 5V PIN8
+    delay(1000);               //Set the delay time, 1000 = 1S
+    digitalWrite(ledPinY,LOW);  //LOW is set to about 5V PIN8
+    delay(1000);  
+    digitalWrite(ledPinG,HIGH); //HIGH is set to about 5V PIN8
+    delay(1000);               //Set the delay time, 1000 = 1S
+    digitalWrite(ledPinG,LOW);  //LOW is set to about 5V PIN8
+    delay(1000);
+    digitalWrite(ledPinY,HIGH); //HIGH is set to about 5V PIN8
+    delay(1000);               //Set the delay time, 1000 = 1S
+    digitalWrite(ledPinY,LOW);  //LOW is set to about 5V PIN8
+    delay(1000); 
+    digitalWrite(ledPinR,HIGH); //HIGH is set to about 5V PIN8
+    delay(1000);               //Set the delay time, 1000 = 1S
+    digitalWrite(ledPinR,LOW);  //LOW is set to about 5V PIN8
+    delay(1000);              //Set the delay time, 1000 = 1S
+}
