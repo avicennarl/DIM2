0b0275440c6843feb2ec6b8424ee9be27c73f147
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-05-21 02:52:47 +0700

    Update 02-LEDpinRYG.ino

--- Arduino言語/02-LEDpinRYG.ino
+++ Arduino言語/02-LEDpinRYG.ino
@@ -8,12 +8,10 @@ void setup()
     pinMode(ledPinR,OUTPUT);
     pinMode(ledPinY,OUTPUT);
     pinMode(ledPinG,OUTPUT);//Set the digital 8 port mode, OUTPUT: Output mode
-
 }
 
 void loop()
 {  
-
     digitalWrite(ledPinR,HIGH); //HIGH is set to about 5V PIN8
     delay(1000);               //Set the delay time, 1000 = 1S
     digitalWrite(ledPinR,LOW);  //LOW is set to about 5V PIN8
