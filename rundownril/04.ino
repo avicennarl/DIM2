de2f6d83b1a8e6ceb003b48e51dd8cbcf18bcfbe
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-05-23 00:02:24 +0700

    last commit 04.ino

---
+++ rundownril/04.ino
@@ -0,0 +1,13 @@
+int ledPin = 4;
+int readValue = 0;
+int ledValue = 0;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop(){
+  readValue = analogRead(A0);
+  ledValue = map(readValue, 0, 1024, 0, 255);
+  analogWrite(ledPin, ledValue);
+}
