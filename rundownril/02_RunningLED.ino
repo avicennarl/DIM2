9c5a60d9c0ae6a184a8f9ddc572644f072eda14e
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-05-22 23:21:10 +0700

    adding last commit 02_RunningLED.ino

---
+++ rundownril/02_RunningLED.ino
@@ -0,0 +1,74 @@
+void setup() {
+  // running led
+  pinMode (2, OUTPUT);
+  pinMode (4, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (8, OUTPUT);
+  pinMode (10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(10, LOW);  
+  delay(1000);
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(10, LOW);  
+  delay(1000);
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(10, LOW);  
+  delay(1000);
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(10, LOW);  
+  delay(1000);
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(10, HIGH);  
+  delay(1000);
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(10, LOW);  
+  delay(1000);  
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(10, LOW);  
+  delay(1000);  
+
+  digitalWrite(2, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(10, LOW);  
+  delay(1000);
+
+  digitalWrite(2, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(10, LOW);  
+  delay(1000);
+}
